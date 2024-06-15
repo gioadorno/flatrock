cdc7d2724724f336ea52189fb77c590c41be38d6
Gio <adorno.gio@gmail.com>
2024-06-14 19:29:44 -0700

    Implement handler

---
+++ handler/handler.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Page struct{
+  c echo.Context
+  page int
+}
+
+func RenderPage(p Page) error {
+  pageNumber := p.c.Param("page")
+  return p.c.Render(http.StatusOK, fmt.Sprintf("page%s", pageNumber), nil)
+}
