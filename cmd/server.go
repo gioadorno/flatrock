a314a2b4bcd1d4247bfdfa65c28870b36e987ee8
gioadorno <adorno.gio@gmail.com>
2024-06-19 20:48:59 -0700

    Update server

--- cmd/server.go
+++ cmd/server.go
@@ -1,16 +1,58 @@
 package main
 
 import (
+	"github.com/gioadorno/flatrock/handler"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
-	e.Use(middleware.Logger())
+	p1 := &Page{c: e.Context(), page: 1}
+	p2 := &Page{c: e.Context(), page: 2}
+	p3 := &Page{c: e.Context(), page: 3}
 
-	err := e.Start(":8080")
-	if err != nil {
-		e.Logger.Fatal(err)
-	}
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "home", nil)
+	})
+
+	e.GET("/page-1", func(c echo.Context) error { return p1.RenderPage() })
+	e.GET("/page-2", func(c echo.Context) error { return p2.Render() })
+	e.GET("/page-3", func(c echo.Context) error { return p3.Render() })
+
+	// hx-get and hx-push-state are used to enable htmx routing
+	e.GET("/page-{page}", func(c echo.Context) error {
+		page := c.Param("page")
+		switch page {
+		case "1":
+			return p1.Render()
+		case "2":
+			return p2.Render()
+		case "3":
+			return p3.Render()
+		default:
+			return c.String(404, "Page not found")
+		}
+	})
+
+	e.GET("/page-{page}/prev", func(c echo.Context) error {
+		page := c.Param("page")
+		switch page {
+		case "1":
+			return p3.Render()
+		default:
+			return p2.Render()
+		}
+	})
+
+	e.GET("/page-{page}/next", func(c echo.Context) error {
+		page := c.Param("page")
+		switch page {
+		case "3":
+			return p1.Render()
+		default:
+			return p2.Render()
+		}
+	})
+
+	e.Start(":8080")
 }
