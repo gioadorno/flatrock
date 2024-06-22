299168ca47ffcdd2d4f501caf5110cd69693164c
gioadorno <adorno.gio@gmail.com>
2024-06-21 20:44:58 -0700

    Update handler

--- handler/handler.go
+++ handler/handler.go
@@ -4,15 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/labstack/echo/v4"
+	"github.com/gioadorno/flatrock/models"
 )
 
-type Page struct {
-  c echo.Context
-  page int
-}
-
-func RenderPage(p Page) error {
+func RenderPage(p models.Page) error {
   pageNumber := p.c.Param("page")
   return p.c.Render(http.StatusOK, fmt.Sprintf("page%s", pageNumber), nil)
 }
